4381f177fce3ca4340812c55603bbc9ce76b4e13
JaineSantos0 <jainejosiane@gmail.com>
2024-10-18 11:34:55 -0300

    feat: creating getReport and getSummary

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,17 @@
 package com.devsuperior.dsmeta.controllers;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -23,15 +26,24 @@ public class SaleController {
 		return ResponseEntity.ok(dto);
 	}
 
+
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleReportDTO>> getReport(
+			Pageable pageable,
+			@RequestParam(value = "minDate", required = false, defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", required = false, defaultValue = "") String maxDate,
+			@RequestParam(value = "name", required = false, defaultValue = "") String name
+	) {
+		Page<SaleReportDTO> salesReports = service.findAllSales(pageable, minDate, maxDate, name);
+		return ResponseEntity.ok(salesReports);
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity<List<SaleSummaryDTO>> getSummary(
+			@RequestParam(value = "minDate", required = false, defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", required = false, defaultValue = "") String maxDate
+	) {
+		List<SaleSummaryDTO> sallesBySeller = service.findTotalSalesBySeller(minDate, maxDate);
+		return ResponseEntity.ok(sallesBySeller);
 	}
 }
